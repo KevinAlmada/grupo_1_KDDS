dee9e36b579f34d070371346f477206df1270200
Dario Quispe <daro.qp@gmail.com>
2021-07-14 22:13:39 -0300

    Nuevo home -falta terminar-

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,37 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1;
+startSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(s){
+    startSlides(slideIndex += s);
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function startSlides(s) {
+    let i;
+    let slides = document.getElementsByClassName("slider");
+
+    if(s > slides.length) slideIndex = 1;
+    if(s < 1) slideIndex = slides.length;
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+let slideBotIndex = 0;
+startSlidesBottom();
+
+function startSlidesBottom(){
+    let i;
+    let slides = document.getElementsByClassName("slider-bottom");
+
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideBotIndex++;
+    if(slideBotIndex > slides.length) slideBotIndex = 1;
+    slides[slideBotIndex - 1].style.display = "block";
+    setTimeout(startSlidesBottom, 3000);
 }
