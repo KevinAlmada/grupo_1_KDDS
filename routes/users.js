903877c45bcdd741239d3f67ba66c4772cb346da
KevinAlmada <kevinalmada81@gmail.com>
2021-08-02 22:29:10 -0300

    fixes

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/cart', (req, res) => {
-  res.sendFile(path.join(__dirname, ".././views/cart.html"))
-});
-
-module.exports = router;
