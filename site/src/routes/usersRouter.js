4438efc8309281d0b6d9c7dec1dd2b481fc15d57
KevinAlmada <kevinalmada81@gmail.com>
2021-08-25 16:35:21 -0300

    register funcional

--- site/src/routes/usersRouter.js
+++ site/src/routes/usersRouter.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/userController')
+var registerValidation = require('../middlewares/registerValidation')
 
 /* GET home page. */
 router.get('/login', controller.login);
+
 router.get('/register', controller.register);
-router.get('/cart', controller.cart);
+router.post('/register',registerValidation, controller.processRegister);
+
+
+router.get('/cart',controller.cart);
 
 module.exports = router;
 
