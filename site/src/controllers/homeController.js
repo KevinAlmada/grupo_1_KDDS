02c600dc5cfd057212931d404320820f9eee786a
KevinAlmada <kevinalmada81@gmail.com>
2021-09-06 14:54:12 -0300

    fix home

--- site/src/controllers/homeController.js
+++ site/src/controllers/homeController.js
@@ -6,11 +6,11 @@ module.exports = {
         const cardDown = [];
         
         for(let i = 0; i < 4; i++){
-            let aleatorio = Math.floor((Math.random() * (19-1))+1);; 
+            let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
             cardUp.push(productdb[aleatorio]);
         }
         for(let i = 0; i < 4; i++){
-            let aleatorio = Math.floor((Math.random() * (19-1))+1);; 
+            let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
             cardDown.push(productdb[aleatorio]);
         }
 
