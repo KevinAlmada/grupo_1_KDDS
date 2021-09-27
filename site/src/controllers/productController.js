2c58b3de27ec6098e0072ebbe9f516e5308781ec
KevinAlmada <kevinalmada81@gmail.com>
2021-09-27 13:00:20 -0300

    products funcionales con db

--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -1,77 +1,83 @@
-let {productdb,userdb} = require('../data/productDb')
+const db = require('../database/models')
+const { Op } = require("sequelize");
+
 module.exports = {
     productDetail:(req,res)=>{
-        let idProd = req.params.id
-        let product = productdb.find(producto => producto.id === +idProd)
-        let productsSim = productdb.filter(producto => producto.category === product.category)
-        if (product) {
-            res.render('product',{
-                db:product,
-                title:product.name,
-                productsSim,
-                usuario:req.session.user?req.session.user:""
+        db.Products.findByPk(req.params.id,{include:[{association:"category"},{association:"productImages"}]})
+            .then(product => {
+                db.Products.findAll({where:{categoryId:product.categoryId},include:[{association:"category"},{association:"productImages"}]})
+                .then(productsSim => {
+                    if (product) {
+                        
+                        res.render('product',{
+                            db:product,
+                            title:product.name,
+                            productsSim,
+                            usuario:req.session.user?req.session.user:""
+                        })
+                    }else{
+                        res.render('error',{
+                            title:`${req.params.id}no existe`,
+                            usuario:req.session.user?req.session.user:""
+                        })
+                    }
+                })
             })
-        }else{
-            res.render('error',{
-                title:`${idProd}no existe`,
-                usuario:req.session.user?req.session.user:""
-            })
-        } 
+            .catch(err => console.log(err))
     },
     products:(req,res)=>{
-        res.render('searchResults',{
-            title : "Productos", 
-            db : productdb,
-            usuario:req.session.user?req.session.user:""
+        db.Products.findAll({include:[{association:"category"},{association:"productImages"}]})
+        .then(db => {
+            res.render('searchResults',{
+                title : "Productos", 
+                db,
+                usuario:req.session.user?req.session.user:""
+            })
         })
     },
     category:(req,res)=>{
-        let categoria = req.params.category
-        let filtrado = productdb.filter(producto=>producto.category == categoria)
-        if (filtrado) {
-            res.render('searchResults',
-            {db : filtrado,
-            title :`Anteojos de ${categoria}`,
-            usuario:req.session.user?req.session.user:""})
-        }else{
-            res.render('error',{
-                title:`${categoria}no existe`,
-                usuario:req.session.user?req.session.user:""
+        db.Products.findAll({include:[{association:"category"},{association:"productImages"}],where:{categoryId:req.params.category}})
+            .then(db => {
+                if (db) {
+                    res.render('searchResults',
+                    {db,
+                    title :`Anteojos de ${db[0].category.name}`,
+                    usuario:req.session.user?req.session.user:""})
+                }else{
+                    res.render('error',{
+                        title:`${req.params.category}no existe`,
+                        usuario:req.session.user?req.session.user:""
+                    })
+                }
             })
-        }
+        
     },ofertas:(req,res)=>{
-        let productsOfert = productdb.filter(producto => producto.discount >= 10)
-        if(productsOfert){
-            res.render('searchResults',
-            {db : productsOfert,
-            title :"ofertas",
-            usuario:req.session.user?req.session.user:""})
-        }else{
-            res.render('error',
-            {usuario:req.session.user?req.session.user:""})
-        }
+        db.Products.findAll({include:[{association:"category"},{association:"productImages"}],where:{discount:{[Op.gt]:10}}})
+        .then(db => {
+            if(db){
+                res.render('searchResults',
+                {db,
+                title :"ofertas",
+                usuario:req.session.user?req.session.user:""})
+            }else{
+                res.render('error',
+                {usuario:req.session.user?req.session.user:""})
+            }
+        })
     },
     busqueda:(req,res)=>{
-        let busqueda = req.query.buscador.trim().toLowerCase()
-        let coincidencias = []
-        let filtradoBuscador = productdb.forEach(prod => {
-            let description = prod.description.toLowerCase().split(" ")
-
-            description.forEach(palabra => {
-                    if (palabra == busqueda && !coincidencias.includes(prod)) {
-                        coincidencias.push(prod)
-                    }
-            }) 
-        })
-        
-        if (coincidencias.length > 0) {
-            res.render('searchResults',
-            {db : coincidencias,
-            title : `resultado de busqueda de ${busqueda}`,
-            usuario:req.session.user?req.session.user:""})
-        }else{
-            res.render('error', {title : `El producto ${busqueda} no existe`,
-            usuario:req.session.user?req.session.user:""} )
-        } 
-    }
-}
+        let busqueda = req.query.buscador.trim().toLowerCase();
+    
+        db.Products.findAll({include:[{association:"category"},{association:"productImages"}],where:{description:{[Op.like]:`%${busqueda}%`}}})
+        .then(db => {
+            if (db.length > 0) {
+                res.render('searchResults',
+                {db ,
+                title : `resultado de busqueda de ${busqueda}`,
+                usuario:req.session.user?req.session.user:""})
+            }else{
+                res.render('error', {title : `No hay resultados para la busqueda de ${busqueda} `,
+                usuario:req.session.user?req.session.user:""} )
+            } 
+        }) 
+}}
