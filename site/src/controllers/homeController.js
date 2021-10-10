186a762cc2bd5c252d68bf8ecdb4f5123c2e27a0
KevinAlmada <kevinalmada81@gmail.com>
2021-10-10 19:48:15 -0300

    fix home problem

--- site/src/controllers/homeController.js
+++ site/src/controllers/homeController.js
@@ -5,31 +5,31 @@ let {productdb,userdb} = require('../data/productDb')
 const Category = require('../database/models/Category')
 module.exports = {
     index:(req,res)=>{
-        /* db.Products.findAll({
-            include:[{association:"category",
-            where:{name:"lectura"}
-    }]
+         db.Products.findAll({
+            include:[{association:"category"},{association:"productImages"}],
         })
-        .then(result=> res.send(result)) */
-        const cardUp = [];
-        const cardDown = [];
-        
-        for(let i = 0; i < 4; i++){
-            let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
-            cardUp.push(productdb[aleatorio]);
-        }
-        for(let i = 0; i < 4; i++){
-            let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
-            cardDown.push(productdb[aleatorio]);
-        }
-
-        res.render('home',{
-            title : "KDDS",
-            db:productdb,
-            cardUp,
-            cardDown,
-            usuario:req.session.user?req.session.user:""
+        .then(productdb => {
+            const cardUp = [];
+            const cardDown = [];
+            
+            for(let i = 0; i < 4; i++){
+                let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
+                cardUp.push(productdb[aleatorio]);
+            }
+            for(let i = 0; i < 4; i++){
+                let aleatorio = Math.floor((Math.random() * (productdb.length-1))+1);; 
+                cardDown.push(productdb[aleatorio]);
+            }
+    
+            res.render('home',{
+                title : "KDDS",
+                db:productdb,
+                cardUp,
+                cardDown,
+                usuario:req.session.user?req.session.user:""
+            }) 
         }) 
+       
     },
     aboutUs: (req,res)=>{
         res.render('about',{
