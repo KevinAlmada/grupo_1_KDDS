b1e0d1241b0e91393891b324fba5bb4534f22f48
KevinAlmada <kevinalmada81@gmail.com>
2021-10-31 21:26:26 -0300

    Header mobile functional , fix home , fix search result

--- site/public/javascripts/burger.js
+++ site/public/javascripts/burger.js
@@ -1,36 +1,13 @@
-let menu = document.getElementById("burger");
-let iconBurger = document.getElementById("hamburguesa");
-let iconCross = document.getElementById("cruz");
 let menuLateral = document.getElementById("menuLateral")
-let panelMenuLateral = document.getElementById("panelMenuLateral")
+let hamburger = document.querySelector(".hamburger");
 window.addEventListener("load", () => {
-    menu.onclick = () => {
-        menuLateral.classList.add("menu-deploy")
-        /* iconCross.style.color = "white"
-        iconCross.style.display = "block" */
-        iconBurger.style.display = "none"
-}        
-
-    iconCross.onclick = () => {
-        menuLateral.classList.remove("menu-deploy")
-         menuLateral.style.transition = "all 0.8s"
-        iconBurger.style.display = "block"
-}
-/* menu.onclick = () => {
-        menuLateral.style.opacity = 1
-        menuLateral.style.left = "0%"
-        iconCross.style.color = "white"
-        iconCross.style.display = "block"
-        iconBurger.style.display = "none"
-}        
-
-    iconCross.onclick = () => {
-        menuLateral.style.opacity = "0"
-        menuLateral.style.left = "-70%"
+    
+    hamburger.onclick = () => {
+        hamburger.classList.toggle("is-active");
+        menuLateral.classList.toggle("menu-deploy")
         menuLateral.style.transition = "all 0.8s"
-        iconCross.style.display = "none"
-        iconBurger.style.display = "block"
-} */
+}     
+
 
 let chevron = document.getElementById("chevron")
 let products = document.getElementById("header-mobile-drop-title");
