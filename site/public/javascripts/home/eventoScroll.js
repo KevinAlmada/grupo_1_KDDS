659450253496c40983436a2e23859a9de8ff212f
dariomamani48 <dariomamani48@gmail.com>
2021-11-15 09:43:29 -0300

    corregido errores de consola enjs scrol

--- site/public/javascripts/home/eventoScroll.js
+++ site/public/javascripts/home/eventoScroll.js
@@ -10,15 +10,15 @@ let $flecha = document.querySelector('.fa-arrow-alt-circle-up')
 if(window.scrollY != 0){
     $minHe.classList.add('scroll')
     $logoHm.style.opacity = '1'
-    $vContainer.classList.add('scrollBanner')
-    $flecha.style.opacity = "1"
-    $vbanner.classList.add('scrollVideo')
+    $vContainer? $vContainer.classList.add('scrollBanner') : " "
+    $flecha ? $flecha.style.opacity = "1" : " "
+    $vbanner ? $vbanner.classList.add('scrollVideo') : " "
 }
 else{
     $minHe.classList.remove('scroll')
     $logoHm.style.opacity = '0'
-    $flecha.style.opacity = '0'
-    $vContainer.classList.remove('scrollBanner')
-    $vbanner.classList.remove('scrollVideo')
+    $flecha ? $flecha.style.opacity = '0' : " "
+    $vContainer ? $vContainer.classList.remove('scrollBanner') : " "
+    $vbanner ? $vbanner.classList.remove('scrollVideo') : " "
 }
 })
